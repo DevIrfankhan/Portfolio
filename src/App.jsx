103a6a65e7aafe6c676afcfce044481fb49ffa7d
Irfan Khan <irfankhanofficial140@gmail.com>
2026-08-13 15:16:04 +0530

    New Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import Project from "./components/Project";
+// import Project from "./components/Project";
 // import NewNav from "./components/NewNav";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <NewNav /> */}
       <Hero />
-      {/* <Project/> */}
-      
-
-      <div className="h-screen flex justify-center items-center text-4xl">
-        Welcome to My Portfolio
-
-      </div>
     </>
   );
 }
