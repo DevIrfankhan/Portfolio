5fb6a91df1cf83f2a2ccc8b12f6a9ab0de05e771
Irfan Khan <irfankhanofficial140@gmail.com>
2026-09-23 20:07:06 +0530

    New Update

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,11 +4,25 @@ import Navbar from "./Navbar";
 import Project from "./Project";
 import Skills from "./Skills";
 import Contact from "./Contact";
+import { useRef } from "react";
 
 const Main = () => {
+    const HeroRef = useRef(null)
+    const ProjectRef = useRef(null)
+    const SkillsRef = useRef(null)
+    const ContactRef = useRef(null)
+    const scrollSection = (ref) => {
+        ref.current?.scrollIntoView({
+           behavior: "smooth"
+       })
+   }
   return (
       <>
           <Navbar
+              onHeroClick={() => scrollSection(HeroRef)}
+              onPeojectClick={() => scrollSection(ProjectRef)}
+              onSkillsClick={() => scrollSection(SkillsRef)}
+              onContactClick={() => scrollSection(ContactRef)}
           />
           <Hero />
           <Project />
